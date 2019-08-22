e6114cca2258282a37bf0dc46041510202ec0c08
sshaul <sshaul@calpoly.edu>
2019-08-21 17:49:00 -0700

    Removed old file comment

--- components/loadingPortfolio.js
+++ components/loadingPortfolio.js
@@ -3,7 +3,6 @@ import { View } from 'react-native';
 import SpinningLoader from './spinningloader';
 import styles from '../style/components/loadingPortfolio';
 
-//------------------------------STILL AN OLD FILE-------------------------------------//
 const LoadingPortfolio = () => {
   return (
     <View style={styles.background}>
