87830c6bd77c9dbee0b98e438fa00492008bd432
asnewman <ashznewman@gmail.com>
2020-02-01 11:31:59 -0800

    made the filtering beautiful

--- screens/search.js
+++ screens/search.js
@@ -1,59 +1,61 @@
-import React, { Component } from 'react';
-import {
-  View, Animated, TouchableOpacity, FlatList, Text,
-} from 'react-native';
-import { Actions } from 'react-native-router-flux';
-import { Feather } from '@expo/vector-icons';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { createFilter } from 'react-native-search-filter';
-import styles from '../style/screens/search';
-import NavBar from '../components/navbar';
-import FormInput from '../components/formInput';
+import React, { Component } from "react";
+import { View, Animated, TouchableOpacity, FlatList, Text } from "react-native";
+import { Actions } from "react-native-router-flux";
+import { Feather } from "@expo/vector-icons";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { createFilter } from "react-native-search-filter";
+import styles from "../style/screens/search";
+import NavBar from "../components/navbar";
+import FormInput from "../components/formInput";
 
 const stocks = [
-  { name: 'Avaya Holdings Corp.', ticker: 'AVH' },
-  { name: 'Abraxas Petroleum Corporation' },
-  { name: 'Axalta Coating Systems Ltd.' },
-  { name: 'Baidu, Inc.', ticker: 'BIDU' },
-  { name: 'Franklin Resources, Inc.' },
-  { name: 'Best Buy Co., Inc.' },
-  { name: 'BioCryst Pharmaceuticals, Inc.' },
-  { name: 'Citigroup Inc' },
-  { name: 'Conagra Brands Inc' },
-  { name: 'Cango Inc ADR' },
-  { name: 'Companhia Brasileira DE Distribuicao' },
-  { name: 'Community Bank System' },
-  { name: 'Compania Cervecerias Unidas S.A.' },
+  { name: "Avaya Holdings Corp.", ticker: "AVH" },
+  { name: "Abraxas Petroleum Corporation", ticker: "APX" },
+  { name: "Axalta Coating Systems Ltd.", ticker: "ACS" },
+  { name: "Baidu, Inc.", ticker: "BIDU" },
+  { name: "Franklin Resources, Inc.", ticker: "FRI" },
+  { name: "Best Buy Co., Inc.", ticker: "BB" },
+  { name: "BioCryst Pharmaceuticals, Inc.", ticker: "BPI" },
+  { name: "Citigroup Inc", ticker: "C" },
+  { name: "Conagra Brands Inc", ticker: "CBI" },
+  { name: "Cango Inc ADR", ticker: "ADR" },
+  { name: "Companhia Brasileira DE Distribuicao", ticker: "CBDD" },
+  { name: "Community Bank System", ticker: "CBS" },
+  { name: "Compania Cervecerias Unidas S.A.", ticker: "CCU" }
 ];
 
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ticker: '',
+      ticker: "",
       top: new Animated.Value(300),
+      showStockSearch: false
     };
   }
 
-  submitSearch = async (ticker) => {
+  submitSearch = async ticker => {
     const { top } = this.state;
     try {
       if (ticker) {
+        this.setState({ showStockSearch: false });
         Animated.timing(top, {
           duration: 300,
-          toValue: 0,
-        }).start(() => Actions.stock({
-          type: 'replace',
-          ticker,
-        }));
+          toValue: 0
+        }).start(() =>
+          Actions.stock({
+            type: "replace",
+            ticker
+          })
+        );
       }
     } catch (err) {
       alert(`Sorry, ${ticker} is not a supported ticker.`);
     }
   };
 
-  truncateStockName = (stockName) => {
-    const lengthLimit = 25;
+  truncateStockName = stockName => {
+    const lengthLimit = 20;
 
     if (stockName.length < lengthLimit) {
       return stockName;
@@ -63,8 +65,10 @@ class Search extends Component {
   };
 
   render() {
-    const { ticker, top } = this.state;
-    const filteredStocks = stocks.filter(createFilter(ticker, ['name']));
+    const { ticker, top, showStockSearch } = this.state;
+    const filteredStocks = stocks.filter(
+      createFilter(ticker, ["name", "ticker"])
+    );
     return (
       <KeyboardAwareScrollView
         resetScrollToCoords={{ x: 0, y: 0 }}
@@ -77,8 +81,11 @@ class Search extends Component {
           <Animated.View style={[styles.horizontal, { top }]}>
             <FormInput
               type="search"
-              onchange={(newTicker) => {
-                this.setState({ ticker: newTicker.toUpperCase() });
+              onchange={newTicker => {
+                this.setState({
+                  ticker: newTicker.toUpperCase(),
+                  showStockSearch: true
+                });
               }}
               value={ticker}
               returnKeyType="done"
@@ -93,24 +100,30 @@ class Search extends Component {
               <Feather name="search" size={24} color="white" />
             </TouchableOpacity>
           </Animated.View>
-          <View style={styles.stockList}>
-            <FlatList
-              data={filteredStocks}
-              renderItem={({ item }) => (
-                <TouchableOpacity
-                  key={item.name}
-                  onPress={() => {
-                    this.submitSearch(item.ticker);
-                  }}
-                >
-                  <Text style={styles.stockListText}>
-                    {this.truncateStockName(item.name)}
-                  </Text>
-                </TouchableOpacity>
-              )}
-              keyExtractor={(stock) => stock.name}
-            />
-          </View>
+          {showStockSearch ? (
+            <View style={styles.stockList}>
+              <FlatList
+                data={filteredStocks}
+                renderItem={({ item }) => (
+                  <TouchableOpacity
+                    key={item.name}
+                    onPress={() => {
+                      this.submitSearch(item.ticker);
+                    }}
+                    style={styles.stockListStock}
+                  >
+                    <Text style={styles.stockListText}>
+                      {this.truncateStockName(item.name)}
+                    </Text>
+                    <Text style={styles.stockListSubtext}>
+                      {this.truncateStockName(item.ticker)}
+                    </Text>
+                  </TouchableOpacity>
+                )}
+                keyExtractor={stock => stock.name}
+              />
+            </View>
+          ) : null}
         </View>
       </KeyboardAwareScrollView>
     );
--- style/screens/search.js
+++ style/screens/search.js
@@ -1,36 +1,57 @@
-import { StyleSheet } from 'react-native';
-import colors from '../colors';
+import { StyleSheet } from "react-native";
+import colors from "../colors";
 
 export default StyleSheet.create({
   background: {
     flex: 1,
     backgroundColor: colors.dark,
-    alignItems: 'center',
+    alignItems: "center"
   },
   content: {
     flex: 0.9,
-    justifyContent: 'center',
+    justifyContent: "center"
   },
   horizontal: {
-    flexDirection: 'row',
-    position: 'absolute',
+    flexDirection: "row",
+    position: "absolute",
     top: 300,
     bottom: 0,
-    left: -147,
+    left: -147
   },
   searchButton: {
-    marginTop: 20,
+    marginTop: 20
   },
   stockList: {
-    flexDirection: 'row',
-    position: 'absolute',
-    top: 365,
-    left: -130,
+    width: 250,
+    backgroundColor: colors.grey,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    elevation: 6,
+    borderRadius: 5,
+    flexDirection: "row",
+    position: "absolute",
+    top: 360,
+    left: -140,
     bottom: 0,
-    height: 200,
+    height: 300
+  },
+  stockListStock: {
+    marginBottom: 10,
+    padding: 10
+    // borderBottomColor: colors.ultraLightGrey,
+    // borderBottomWidth: 1
   },
   stockListText: {
-    color: colors.white,
-    fontSize: 20,
+    color: colors.bright,
+    fontSize: 20
   },
+  stockListSubtext: {
+    color: colors.white,
+    fontSize: 12
+  }
 });
